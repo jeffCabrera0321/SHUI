697b95fc8d05e6c640134294cd85c2214ff6f671
jeffCabrera0321 <j.cabrera0321@gmail.com>
2024-08-11 23:44:40 -0400

    initial creation of Smart Homie UI

---
+++ src/pages/signin/index.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function SigninPage() {
+    return(
+        <main>
+            <Link href="/"></Link>
+            <h1>Hello</h1>
+        </main>
+    );
+
+}
